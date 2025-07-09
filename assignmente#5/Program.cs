d9f86585ac4f70737a941c8240eea111daf8e5bf
itahmedfarouk <itahmedfarouk@gmail.com>
2025-07-09 23:12:20 +0300

    Add Assignment 05 solution

---
+++ assignmente#5/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        #region Q19
+        // Write a program that prints an identity matrix using for loop.
+        Console.WriteLine("Q19: Write a program that prints an identity matrix using for loop.");
+        #endregion
+
+        #region Q20
+        // Write a program in C# Sharp to find the sum of all elements of the array.
+        Console.WriteLine("Q20: Write a program in C# Sharp to find the sum of all elements of the array.");
+        #endregion
+
+        #region Q21
+        // Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+        Console.WriteLine("Q21: Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.");
+        #endregion
+
+        #region Q22
+        // Write a program in C# Sharp to count the frequency of each element of an array.
+        Console.WriteLine("Q22: Write a program in C# Sharp to count the frequency of each element of an array.");
+        #endregion
+
+        #region Q23
+        // Write a program in C# Sharp to find maximum and minimum element in an array.
+        Console.WriteLine("Q23: Write a program in C# Sharp to find maximum and minimum element in an array.");
+        #endregion
+
+        #region Q24
+        // Write a program in C# Sharp to find the second largest element in an array.
+        Console.WriteLine("Q24: Write a program in C# Sharp to find the second largest element in an array.");
+        #endregion
+
+        #region Q25
+        // Write a program to find the longest distance between two equal cells in an array.
+        Console.WriteLine("Q25: Write a program to find the longest distance between two equal cells in an array.");
+        #endregion
+
+        #region Q26
+        // Given a list of space separated words, reverse the order of the words.
+        Console.WriteLine("Q26: Given a list of space separated words, reverse the order of the words.");
+        #endregion
+
+        #region Q27
+        // Write a program to create two multidimensional arrays of same size, copy values from first to second, and print the second.
+        Console.WriteLine("Q27: Write a program to create two multidimensional arrays of same size, copy values from first to second, and print the second.");
+        #endregion
+
+        #region Q28
+        // Write a Program to Print One Dimensional Array in Reverse Order.
+        Console.WriteLine("Q28: Write a Program to Print One Dimensional Array in Reverse Order.");
+        #endregion
+
+    }
+}
