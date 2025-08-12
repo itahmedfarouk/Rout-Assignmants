fd38bccefa0a5d829a193baffea741ac797dbf17
itahmedfarouk <itahmedfarouk@gmail.com>
2025-08-12 23:58:28 +0300

    Add Assignment 12 folder

---
+++ Assignment 12/Assignment 12/ParenthesesChecker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ParenthesesChecker
+{
+    public static bool IsBalanced(string input)
+    {
+        Stack<char> stack = new Stack<char>();
+
+        foreach (char ch in input)
+        {
+            if (ch == '(' || ch == '{' || ch == '[')
+            {
+                stack.Push(ch);
+            }
+            else if (ch == ')' || ch == '}' || ch == ']')
+            {
+                if (stack.Count == 0) return false;
+
+                char top = stack.Pop();
+
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '['))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
